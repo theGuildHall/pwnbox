716335d54c1dae46b3d67c84da93b4d72f0ff0c9
theGuildHall <jxg975@gmail.com>
2020-09-07 10:36:00 -0600

    Updated vpnserver.sh with enhancements
    With multiple hacking platforms that require VPN access, I wanted to carry over the IP and server information in my bash prompt even if I wasn't using hackthebox (like tryhackme). Playing around with the vpnserver.sh, we can set it up so that if you cannot to any VPN server, it will add in the server and IP into your terminal.
    
    On your linux machine, you can have multiple openvpn `conf` files in your `/etc/openvpn` directory.
    For example, on my machine, i downloaded the HTB VPN connection and also the TryHackMe vpn connection to my machine, and then moved them over to `/etc/openvpn`.
    
    `sudo mv theguildhall.ovpn /etc/openvpn/theguildhall.conf`
    `sudo mv thm.ovpn /etc/openvpn/thm.conf`
    
    Now with both files in my `/etc/openvpn` directory, I can start the VPN connection when I wish:
    
    `sudo systemctl start openvpn@thm.service`
    
    to stop:
    
    `sudo systemctl stop openvpn@thm.service`
    
    And If I want to start my htb vpn, I just replace the `thm.service` with `theguildhall.service` (or to whatever you named it).
    
    I even created some aliases in my `.bashrc` file to quickly stop and connect:
    
    ```bash
    HTB='sudo systemctl start openvpn@theguildhall.service'
    THM='sudo systemctl start openvpn@thm.service'
    stop='sudo systemctl stop openvpn@*'
    ```
    
    So if I want to connect to HTB, I just type in `HTB` and it connects.

--- vpnserver.sh
+++ vpnserver.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-cat /etc/openvpn/*.conf | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-
+
+#cat /etc/openvpn/*.conf | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-
+
+vpn=$(cat `systemctl status openvpn@* | grep "/usr/sbin/openvpn" | tr -s " " | cut -d " " -f 12` | grep "remote " | cut -d " " -f 2)
+
+if [[ $vpn == *"hackthebox"* ]]
+then
+    cat `systemctl status openvpn@* | grep "/usr/sbin/openvpn" | tr -s " " | cut -d " " -f 12` | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-
+else
+    cat `systemctl status openvpn@* | grep "/usr/sbin/openvpn" | tr -s " " | cut -d " " -f 12` | grep "remote " | cut -d " " -f 2
+fi
