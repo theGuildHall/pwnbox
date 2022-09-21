ee6601429eb9719f5f5282055f942b3fda15a712
N1ght420 <n1ghtpe0ple@protonmail.com>
2022-09-22 05:16:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,9 +31,9 @@ echo -e "$yellow //$white Copying shell configuration"
 cp ~/.$rc ~/.$rc.bak
 cp $rc ~/.$rc
 echo -e "$yellow //$white Copying scripts, sublime text, and bloodhound to /opt"
-sudo cp -r opt /opt
+sudo cp -r opt/ /opt
 echo -e "$yellow //$white Copying wallpapers, icons, and themes to /usr/share"
-sudo cp -r usr /usr
+sudo cp -r usr/ /usr
 echo -e "$yellow //$white Configuring openvpn"
 sudo cp /opt/vpnbash_$configname.sh /opt/vpnbash.sh
 echo -e "$yellow //$white Copying configs"
